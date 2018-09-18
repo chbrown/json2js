3cb20e349922a407286faceed36f277cd6e37f76
Christopher Brown <io@henrian.com>
2018-09-18 17:41:11 -0500

    Add 'util' module with randomIdentifier()

---
+++ util.ts
@@ -0,0 +1,15 @@
+function randomNumber(min: number = 0,
+                      max: number = 1): number {
+  // Math.random() might return 0, but will not return 1
+  return Math.random() * (max - min) + min
+}
+
+/**
+This returns a random string with length == 13, prefixed with '__',
+followed by 11 characters consisting of digits 0-9 and/or lowercase letter a-z.
+*/
+export function randomIdentifier(): string {
+  // parseInt('10000000000', 36) => 3656158440062976
+  const random_positive_integer = Math.round(randomNumber(3656158440062976, Number.MAX_SAFE_INTEGER) + 1)
+  return `__${random_positive_integer.toString(32)}`
+}
